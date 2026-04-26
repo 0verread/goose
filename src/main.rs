c03ac97e6f25c5a12ed22fb577c61b9b5b62df2d
0verread <iamsubhajit.d@gmail.com>
2026-04-26 11:30:18 -0700

    commnads and arch (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "goose", version = "0.1.0", about = "A CLI tool for devs")]
+struct Args {
+    #[clap(subcommand)]
+    subcommand: Subc,
+}
+
+#[derive(Debug, Subcommand)]
+enum Subc {
+    // Push current changes  to remote branch
+    Push {},
+    Switch { branch: String },
+    New { branch: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    if let Subc::Push {} = &args.subcommand {
+        eprintln!("pushing to the head");
+    }
 }
